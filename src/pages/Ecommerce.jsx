89627870364abe308b6485d8e94192e84baed1d1
Herman Adu <herman.adu@hotmail.com>
2022-05-25 10:41:55 +0100

    added second view for the orders page added the header component to application

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -117,6 +117,7 @@ const Ecommerce = () => {
                 />
               </div>
             </div>
+
             <div>
               <Stacked width="320px" height="360px" />
             </div>
